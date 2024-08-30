09785eb529f0670d9a94ce86931e5a6b7e2be1a6
ST5906 <transebastian21@gmail.com>
2024-08-30 10:40:10 -0400

    completed week 1 assignments

--- ex2/src/main/java/Bulbasaur.java
+++ ex2/src/main/java/Bulbasaur.java
@@ -4,56 +4,80 @@ public class Bulbasaur {
 
     // Constructor
     public Bulbasaur() {
-        // TODO: Initialize id and level with appropriate values
+        id = 1;
+        level = 1;
     }
 
     // setLevel method
     public void setLevel(int lv) {
-        // TODO: Implement this method
         // Update the level
+        level = lv;
+
         // If the new level is 16 or higher but less than 32, evolve to Ivysaur
+        if (level >= 16 && level < 32){
+            id = 2;
+        }
+
         // If the new level is 32 or higher, evolve to Venusaur
+        else if (level >= 32){
+            id = 3;
+        }
     }
 
     // getLevel method
     public int getLevel() {
-        // TODO: Implement this method
-        return 0; // Placeholder return value
+        return level;
     }
 
     // getName method
     public String getName() {
-        // TODO: Implement this method
         // Return the name based on the current id
-        return ""; // Placeholder return value
+        switch (id){
+            case 1 : 
+                return "Bulbasaur";
+            case 2 : 
+                return "Ivysaur";
+            case 3 : 
+                return "Venusaur";
+            default:
+                return "";
+        }
     }
 
     // getID method
     public int getID() {
-        // TODO: Implement this method
-        return 0; // Placeholder return value
+        return id;
     }
 
     // toString method
     @Override
     public String toString() {
-        // TODO: Implement this method
         // Return a string representation of the Bulbasaur object
-        return ""; // Placeholder return value
+        return "level : " + level + "\tid : " + id ; // Placeholder return value
     }
 
     // equals method
     @Override
     public boolean equals(Object obj) {
-        // TODO: Implement this method
         // Compare this Bulbasaur object with another object
-        return false; // Placeholder return value
+        if (obj instanceof Bulbasaur){
+            Bulbasaur cast = (Bulbasaur) obj;
+            if (cast.level == level && cast.id == id){
+                return true;
+            }
+            return false;
+        }
+        else{
+            return false;
+        }
+
     }
 
     // copy method
     public Bulbasaur copy() {
-        // TODO: Implement this method
-        // Create and return a new Bulbasaur object with the same id and level
-        return null; // Placeholder return value
+        Bulbasaur copy = new Bulbasaur();
+        copy.id = id;
+        copy.level = level;
+        return copy;
     }
 }
